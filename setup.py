689c14364b7ea794f825f45cdc929be7b37f54a9
Augusto Augusto <avilelateixeira@gmail.com>
2021-01-03 16:20:35 -0300

    Adding long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='sauce_storage_api',
     author='Augusto Vilela',
     author_email='avilelateixeira@gmail.com',
     version='1.0.0',
     description='Lib to use saucelabs Application Storage API - https://wiki.saucelabs.com/display/DOCS/Application+Storage',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     keyword=['saucelabs', 'application storage'],
     install_requires=['requests']
